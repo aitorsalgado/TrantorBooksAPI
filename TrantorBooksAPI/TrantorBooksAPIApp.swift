718444fa1740b3d4ddeff9da037d903a8d98c86d
Aitor Salgado <aitorsalgado6@icloud.com>
2023-02-18 11:53:10 +0100

    Initial Commit

---
+++ TrantorBooksAPI/TrantorBooksAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrantorBooksAPIApp.swift
+//  TrantorBooksAPI
+//
+//  Created by Aitor Salgado on 18/2/23.
+//
+
+import SwiftUI
+
+@main
+struct TrantorBooksAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
