ffb9b9327047ef86d989ce7cd35d4ef63b33a498
Aitor Salgado <aitorsalgado6@icloud.com>
2023-02-19 10:58:44 +0100

    Add /books endpoints

--- TrantorBooksAPI/NetworkInterface.swift
+++ TrantorBooksAPI/NetworkInterface.swift
@@ -10,8 +10,20 @@ import Foundation
 let serverURL = URL.productionServer
 
 extension URL {
-    static let productionServer = URL(string: "https://trantorapi-acacademy.herokuapp.com/api/")!
+    static let productionServer = URL(string: "https://trantorapi-acacademy.herokuapp.com/api")!
     
-    static let getBooks = serverURL.appending(component: "list")
-    static let getLatestBooks = serverURL.appending(component: "latest")
+    static let getLatestBooks = serverURL.appending(component: "books").appending(component: "latest")
+    static let getBooks = serverURL.appending(component: "books").appending(component: "list")
+    static let getAuthors = serverURL.appending(component: "books").appending(component: "authors")
+    
+    static func getBooksByTitle(titleToSearch:String) -> URL {
+        //Not necessary lowercased because the API is not case sensitive
+        serverURL.appending(component: "books").appending(component: "find").appending(component: "\(titleToSearch)")
+    }
+    
+    static func getAuthorDetailByID(id: UUID) -> URL {
+        serverURL.appending(component: "books").appending(component: "getAuthor").appending(component: "\(id)")
+    }
+    
+    
 }
