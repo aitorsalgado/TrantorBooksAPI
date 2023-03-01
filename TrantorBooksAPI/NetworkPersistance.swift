01ee97852102b39d6fa98cb4d718890954d6ac59
Aitor Salgado <aitorsalgado6@icloud.com>
2023-03-01 21:51:10 +0100

    Add getAuthorDetailByID first implementation

--- TrantorBooksAPI/NetworkPersistance.swift
+++ TrantorBooksAPI/NetworkPersistance.swift
@@ -14,4 +14,24 @@ final class NetworkPersistance {
         let (data, _) = try await URLSession.shared.data(from: .getLatestBooks)
         return try JSONDecoder().decode([Book].self, from: data)
     }
+    
+    func getAuthorDetailByID(id:UUID) async throws -> AuthorDetail {
+        do {
+            let (data, response) = try await URLSession.shared.data(from: .getAuthorDetailByID(id: id))
+            guard let response = response as? HTTPURLResponse else { throw APIErrors.nonHTTP }
+            if response.statusCode == 200 {
+                do {
+                    return try JSONDecoder().decode(AuthorDetail.self, from: data)
+                } catch {
+                    throw APIErrors.json(error)
+                }
+            } else {
+                throw APIErrors.status(response.statusCode)
+            }
+        } catch let error as APIErrors {
+            throw error
+        } catch {
+            throw APIErrors.general(error)
+        }
+    }
 }
