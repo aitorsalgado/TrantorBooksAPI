5f3d2bb7bf002759facf2a2b4f54594fd6d73148
Aitor Salgado <aitorsalgado6@icloud.com>
2023-03-01 19:18:42 +0100

    Add first UI changes to list of books

--- TrantorBooksAPI/ContentView.swift
+++ TrantorBooksAPI/ContentView.swift
@@ -12,7 +12,37 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List(booksVM.books){ book in
-                Text(book.title)
+                HStack {
+                    AsyncImage(url: book.cover) { image in
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 100)
+                            .clipShape(Rectangle())
+                    } placeholder: {
+                        Image(systemName: "text.book.closed")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 50, height: 100)
+                            .clipShape(Rectangle())
+                            
+                    }
+                    VStack(alignment: .leading) {
+                        Text(book.title)
+                            .font(.headline)
+                        Text(book.author.description)
+                            .font(.caption)
+                    }
+                    Spacer()
+                    Text(book.price.formatted(.currency(code: "EUR").locale(Locale(identifier: "es-ES"))))
+                        .bold()
+                        .padding(5)
+                        .padding(.horizontal, 8)
+                        .background {
+                            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                                .fill(.gray.opacity(0.2))
+                        }
+                }
             }
             .navigationTitle("Trantor Latest Books")
         }
