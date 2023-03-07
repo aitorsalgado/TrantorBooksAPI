5368bd4160cfef356f08519dcb1d9359e22ee02a
Aitor Salgado <aitorsalgado6@icloud.com>
2023-03-07 17:44:56 +0100

    Add Detail View

--- TrantorBooksAPI/BookDetailView.swift
+++ TrantorBooksAPI/BookDetailView.swift
@@ -11,8 +11,52 @@ struct BookDetailView: View {
     @ObservedObject var booksDetailVM:BooksDetailViewModel
     
     var body: some View {
-        Text(booksDetailVM.book.title)
-            .font(.title)
+        ScrollView {
+            VStack {
+                AsyncImage(url: booksDetailVM.book.cover) { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                } placeholder: {
+                    Image(systemName: "text.book.closed")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 400, height: 400)
+                }
+                Spacer()
+                Text(booksDetailVM.book.title)
+                    .font(.title)
+                Text(booksDetailVM.authorName)
+                    .font(.title2)
+                Text(booksDetailVM.book.price.formatted(.currency(code: "EUR").locale(Locale(identifier: "es-ES"))))
+                    .bold()
+                    .padding(5)
+                    .padding(.horizontal, 8)
+                    .background {
+                        RoundedRectangle(cornerRadius: 20, style: .continuous)
+                            .fill(.gray.opacity(0.2))
+                    }
+                Text(booksDetailVM.book.plot ?? "")
+                    .font(.caption)
+                    .lineLimit(10, reservesSpace: true)
+                    .padding()
+                Section {
+                    HStack(spacing: 50) {
+                        VStack {
+                            Text("Length")
+                                .font(.headline)
+                            Text(booksDetailVM.book.pages?.description ?? "0")
+                        }
+                        VStack {
+                            Text("Year")
+                                .font(.headline)
+                            Text(booksDetailVM.book.year.description)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
